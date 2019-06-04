b8984123fd012f20a891563116dec37c956f1d5d
balderi <balderi@tsponline.dk>
2019-06-04 13:38:48 +0200

    Add project files.

---
+++ YoutubeDlWrapper/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace YoutubeDlWrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.tbAddress = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.cbAudio = new System.Windows.Forms.CheckBox();
+            this.lblFormat = new System.Windows.Forms.Label();
+            this.cbFormat = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tbOutput.Cursor = System.Windows.Forms.Cursors.Default;
+            this.tbOutput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOutput.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.tbOutput.Location = new System.Drawing.Point(13, 4);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ReadOnly = true;
+            this.tbOutput.Size = new System.Drawing.Size(505, 53);
+            this.tbOutput.TabIndex = 100;
+            // 
+            // tbAddress
+            // 
+            this.tbAddress.Location = new System.Drawing.Point(13, 73);
+            this.tbAddress.Name = "tbAddress";
+            this.tbAddress.Size = new System.Drawing.Size(424, 20);
+            this.tbAddress.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(444, 72);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 49);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.pBar);
+            this.panel1.Controls.Add(this.cbAudio);
+            this.panel1.Controls.Add(this.lblFormat);
+            this.panel1.Controls.Add(this.cbFormat);
+            this.panel1.Controls.Add(this.tbOutput);
+            this.panel1.Controls.Add(this.tbAddress);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 135);
+            this.panel1.TabIndex = 102;
+            // 
+            // pBar
+            // 
+            this.pBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pBar.Location = new System.Drawing.Point(0, 126);
+            this.pBar.MarqueeAnimationSpeed = 20;
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(525, 5);
+            this.pBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.pBar.TabIndex = 104;
+            // 
+            // cbAudio
+            // 
+            this.cbAudio.AutoSize = true;
+            this.cbAudio.Location = new System.Drawing.Point(13, 101);
+            this.cbAudio.Name = "cbAudio";
+            this.cbAudio.Size = new System.Drawing.Size(76, 17);
+            this.cbAudio.TabIndex = 2;
+            this.cbAudio.Text = "Only audio";
+            this.cbAudio.UseVisualStyleBackColor = true;
+            this.cbAudio.CheckedChanged += new System.EventHandler(this.cbAudio_CheckedChanged);
+            // 
+            // lblFormat
+            // 
+            this.lblFormat.AutoSize = true;
+            this.lblFormat.Location = new System.Drawing.Point(95, 102);
+            this.lblFormat.Name = "lblFormat";
+            this.lblFormat.Size = new System.Drawing.Size(72, 13);
+            this.lblFormat.TabIndex = 103;
+            this.lblFormat.Text = "Select format:";
+            // 
+            // cbFormat
+            // 
+            this.cbFormat.DisplayMember = "1";
+            this.cbFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFormat.FormattingEnabled = true;
+            this.cbFormat.Location = new System.Drawing.Point(173, 99);
+            this.cbFormat.Name = "cbFormat";
+            this.cbFormat.Size = new System.Drawing.Size(264, 21);
+            this.cbFormat.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = YoutubeDlWrapper.Properties.Resources.yt_logo;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 103;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(127, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(359, 50);
+            this.label2.TabIndex = 104;
+            this.label2.Text = "YouTube Download";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(529, 204);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YouTube Download";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.TextBox tbAddress;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox cbAudio;
+        private System.Windows.Forms.Label lblFormat;
+        private System.Windows.Forms.ComboBox cbFormat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar pBar;
+    }
+}
+
---
+++ YoutubeDlWrapper/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace YoutubeDlWrapper
+{
+    public partial class Form1 : Form
+    {
+        string videoArgs = "--add-metadata -c -o %(title)s.%(ext)s --merge-output-format mkv";
+        string audioArgs = "--add-metadata -c -o %(title)s.%(ext)s -x --audio-format";
+
+        public Dictionary<string, string> audioFormats;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            tbOutput.Text = "Checking for updates...";
+            lblFormat.Visible = false;
+            cbFormat.Visible = false;
+            pBar.Visible = false;
+
+            audioFormats = new Dictionary<string, string>();
+            audioFormats.Add("AAC", "aac");
+            audioFormats.Add("MP3", "mp3");
+            audioFormats.Add("Ogg Vorbis", "vorbis");
+            audioFormats.Add("Wave", "wav");
+
+            foreach(KeyValuePair<string, string> format in audioFormats)
+            {
+                cbFormat.Items.Add(format.Key);
+            }
+
+            Process process = new Process();
+            process.StartInfo.FileName = "youtube-dl.exe";
+            process.StartInfo.Arguments = "--update";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.OutputDataReceived += new DataReceivedEventHandler(ytdlOutputHandler);
+            process.Start();
+            process.BeginOutputReadLine();
+        }
+
+        bool CheckForm(out string message)
+        {
+            if (string.IsNullOrWhiteSpace(tbAddress.Text))
+            {
+                message = "No address!";
+                return false;
+            }
+
+            if (!Regex.Match(tbAddress.Text, @"^(https:\/\/www\.youtube\.com\/watch\?v=).+").Success)
+            {
+                message = "Invalid address!";
+                return false;
+            }
+
+            if(cbAudio.Checked)
+            {
+                if(cbFormat.SelectedItem == null)
+                {
+                    message = "Select audio format";
+                    return false;
+                }
+            }
+
+            message = "Success!";
+            return true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(!CheckForm(out string message))
+            {
+                MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string format = "";
+            if (cbAudio.Checked) audioFormats.TryGetValue(cbFormat.SelectedItem.ToString(), out format);
+
+            tbOutput.Text = string.Empty;
+            DisableControls();
+            btnStart.Text = "Wait...";
+            Process process = new Process();
+            process.StartInfo.FileName = "youtube-dl.exe";
+            process.StartInfo.Arguments = cbAudio.Checked ? audioArgs + " " + format + " " + tbAddress.Text : videoArgs + " " + tbAddress.Text;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.OutputDataReceived += new DataReceivedEventHandler(ytdlOutputHandler);
+            process.ErrorDataReceived += (obj, args) => 
+            {
+                if (!string.IsNullOrWhiteSpace(args.Data))
+                {
+                    MessageBox.Show(args.Data, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            };
+            process.ErrorDataReceived += new DataReceivedEventHandler(ytdlErrorHandler);
+            process.Exited += (obj, args) => 
+            {
+                if (process.ExitCode == 0)
+                {
+                    MessageBox.Show("Done!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            };
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.EnableRaisingEvents = true;
+            process.Exited += SomethingHandler;
+        }
+
+        void ytdlOutputHandler(object sender, DataReceivedEventArgs e)
+        {
+            Trace.WriteLine(e.Data);
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                tbOutput.Text = string.IsNullOrWhiteSpace(e.Data) ? tbOutput.Text : e.Data;
+            }));
+        }
+
+        void ytdlErrorHandler(object sender, DataReceivedEventArgs e)
+        {
+            Trace.WriteLine(e.Data);
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                tbOutput.Text = string.IsNullOrWhiteSpace(e.Data) ? tbOutput.Text : e.Data;
+            }));
+        }
+
+        void SomethingHandler(object sender, EventArgs e)
+        {
+            BeginInvoke(new MethodInvoker(() =>
+            {
+                tbOutput.Text = "Ready.";
+                btnStart.Text = "Start";
+                EnableControls();
+                tbAddress.Focus();
+            }));
+        }
+
+        private void cbAudio_CheckedChanged(object sender, EventArgs e)
+        {
+            lblFormat.Visible = cbAudio.Checked;
+            cbFormat.Visible = cbAudio.Checked;
+        }
+
+        void DisableControls()
+        {
+            btnStart.Enabled = false;
+            cbAudio.Enabled = false;
+            cbFormat.Enabled = false;
+            tbAddress.Enabled = false;
+            pBar.Visible = true;
+        }
+
+        void EnableControls()
+        {
+            btnStart.Enabled = true;
+            cbAudio.Enabled = true;
+            cbFormat.Enabled = true;
+            tbAddress.Enabled = true;
+            pBar.Visible = false;
+        }
+    }
+}
